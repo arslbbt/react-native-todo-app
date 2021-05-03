0c4fbb0a8a8b2e57135c4745859ca18f590f4015
Muhammad Arslan <arslbbt@gmail.com>
2021-05-03 21:01:56 +0500

    updates

--- android/app/src/main/java/com/sampletodo/MainApplication.java
+++ android/app/src/main/java/com/sampletodo/MainApplication.java
@@ -7,6 +7,10 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
 //import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativenavigation.NavigationReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativenavigation.NavigationReactPackage;
 //import com.facebook.react.ReactNativeHost;
 //import com.facebook.soloader.SoLoader;
 
